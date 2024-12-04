83e932bc1e8328c6c573f8b73794db60fc774502
Carolyn <nicecarolyn@gmail.com>
2024-12-04 15:57:05 -0500

    product form and list

---
+++ src/src/components/productlist.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function ProductList({ products }) {
+  return (
+    <div>
+      <h2>Product List</h2>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            <strong>{product.name}</strong> - ${product.price} ({product.category})
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default ProductList;
+
